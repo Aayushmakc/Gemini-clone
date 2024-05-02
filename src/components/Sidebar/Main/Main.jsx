7fab58742667e42f7517fb237dce649cde5299c7
Aayushmakc <rojikc764@gmail.com>
2024-05-02 09:23:30 +0545

    Add initial files for Gemini clone project

---
+++ src/components/Sidebar/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css'
+const Main = () => {
+  return (
+    <div className='main'>
+      <div className='nav'>
+        <p>Gemini</p>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
